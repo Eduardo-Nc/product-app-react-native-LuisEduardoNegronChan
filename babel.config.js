8f8d5d480669283f3af9ff9c8d550179a627e936
Eduardo Negron <eduardonegron@MacBook-Air-de-Eduardo.local>
2025-06-04 14:03:39 -0600

    [chore] - Se crearon alias de todos los modulos a utilizar en la App

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,30 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: [
+          '.ios.js',
+          '.android.js',
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.json',
+        ],
+        alias: {
+          '@Redux': './ProductApp/src/redux',
+          '@Languages': './ProductApp/assets/languages',
+          '@Utils': './ProductApp/src/utils',
+          '@Assets': './ProductApp/assets',
+          '@Screens': './ProductApp/src/views',
+          '@Components': './ProductApp/src/components',
+          '@Navigation': './ProductApp/src/navigation',
+          '@Api': './ProductApp/src/api',
+          '@Services': './ProductApp/src/services',
+        },
+      },
+    ],
+  ],
 };
