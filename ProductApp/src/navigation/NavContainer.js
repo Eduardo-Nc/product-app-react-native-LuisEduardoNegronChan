33bb8828a11295e1b3a122b83a2a6792d706a9d6
Eduardo Negron <Eduardo-negron@live.com>
2025-06-04 16:23:25 -0600

    [chore] - Implementacion de alert

--- ProductApp/src/navigation/NavContainer.js
+++ ProductApp/src/navigation/NavContainer.js
@@ -3,6 +3,7 @@ import React from 'react'
 //Librerias
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import FlashMessage from 'react-native-flash-message';
 
 //Navigation
 const Stack = createNativeStackNavigator();
@@ -13,6 +14,7 @@ import { MainStack } from '@Navigation/stack/MainStack';
 const NavContainer = () => {
   return (
     <NavigationContainer>
+      <FlashMessage position="top" />
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="MainStack" component={MainStack} />
       </Stack.Navigator>
