45e76d06a03c84c80d7cb3ec333d72f9c52277c8
Eduardo Negron <Eduardo-negron@live.com>
2025-06-04 19:17:40 -0600

    [refactor] - Se agrego la consulta del detalle del producto

--- ProductApp/src/views/Products/ProductDetails/ProductDetails.Model.jsx
+++ ProductApp/src/views/Products/ProductDetails/ProductDetails.Model.jsx
@@ -1,11 +1,63 @@
-import {} from 'react';
+import {useState, useLayoutEffect} from 'react';
 
 //Librerias
-import {useNavigation} from '@react-navigation/core';
+import {useDispatch} from 'react-redux';
+import {showMessage} from 'react-native-flash-message';
 
-export default function useProductDetailsModel() {
-  //Navigation
-  const navigator = useNavigation();
+//Languages
+import {translate} from '@Languages/I18n';
 
-  return {};
+//Services
+import {getProductById} from '@Services/productsService';
+
+//Redux
+import {setProductsData} from '../redux/actions/index';
+
+export default function useProductDetailsModel({route}) {
+  //Params
+  const {params} = route;
+  const {id} = params;
+
+  //Redux
+  const dispatch = useDispatch();
+
+  //useState
+  const [loading, setLoading] = useState(false);
+  const [product, setProduct] = useState({});
+
+  //functions
+  const findProduct = () => {
+    setLoading(true);
+    getProductById(id)
+      .then(res => {
+        console.log(res);
+        showMessage({
+          message: `${translate('popupMessage.acceptSubTitle')}`,
+          type: 'success',
+        });
+        setProduct(res);
+      })
+      .catch(err => {
+        showMessage({
+          message: `${translate('serverError.InternalError')}`,
+          type: 'danger',
+        });
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  const formattedPrice = price => {
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(parseFloat(price));
+  };
+
+  useLayoutEffect(() => {
+    findProduct();
+  }, []);
+
+  return {loading, setLoading, product, formattedPrice};
 }
--- ProductApp/src/views/Products/ProductDetails/ProductDetails.Screen.jsx
+++ ProductApp/src/views/Products/ProductDetails/ProductDetails.Screen.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import {View, SafeAreaView} from 'react-native';
+import {
+  View,
+  SafeAreaView,
+  Text,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
 
 //Languages
 import {translate} from '@Languages/I18n';
@@ -10,15 +17,52 @@ import useProductDetailsModel from './ProductDetails.Model';
 //Styles
 import {styles} from './ProductDetails.Styles';
 
+//Components
+import Loader from '@Components/Loader/Loader';
+import Header from '@Components/Header/Header';
+
 //Constants
 import {COLORS, ICONS} from '@Utils/constants';
 
-const ProductDetailsScreen = () => {
-  const {} = useProductDetailsModel();
+const ProductDetailsScreen = ({route}) => {
+  const {loading, setLoading, formattedPrice, product} = useProductDetailsModel(
+    {route},
+  );
 
   return (
     <View style={styles.container}>
-      <SafeAreaView style={styles.mainContainer}></SafeAreaView>
+      <Header
+        icon={ICONS.Canasta}
+        title={translate('appName')}
+        goToBack={true}
+      />
+      <SafeAreaView style={styles.mainContainer}>
+        <ScrollView>
+          <Text style={styles.txtTitle}>{product?.title}</Text>
+          {!loading && (
+            <View style={styles.detailsContainer}>
+              <Image style={styles.image} source={{uri: product?.image}} />
+              <Text style={styles.txtDescription}>{product?.description}</Text>
+              <Text style={styles.txtPrice}>
+                {formattedPrice(product?.price)}
+              </Text>
+
+              <TouchableOpacity style={styles.containerAdd}>
+                <Text style={styles.txtAdd}>
+                  {translate('products.update')}
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.containerDelete}>
+                <Text style={styles.txtDelete}>
+                  {translate('products.delete')}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          )}
+        </ScrollView>
+      </SafeAreaView>
+
+      <Loader visible={loading} setVisible={setLoading} />
     </View>
   );
 };
